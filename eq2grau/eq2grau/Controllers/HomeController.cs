7ba880d278f20d5a81378e2b7ab1431673102291
Hugo1506 <aluno24171@ipt.pt>
2023-03-23 16:38:47 +0000

    eq feita

--- eq2grau/eq2grau/Controllers/HomeController.cs
+++ eq2grau/eq2grau/Controllers/HomeController.cs
@@ -13,8 +13,35 @@ namespace eq2grau.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int A, int B, int C)
         {
+            //variavel auxiliar
+            double delta = B * B - 4 * A * C;
+            String x1 = "", x2 = "";
+            //validar se há condições para efetuar o calculo
+            if (A != 0)
+            {
+                if (delta <= 0)
+                {
+                    //variaveis auxiliares
+                    x1 = (-B + Math.Sqrt(delta)) / 2 / A + "";
+                    x2 = (-B - Math.Sqrt(delta)) / 2 / A + "";
+                }
+                else
+                {
+                    x1 = -B / (2 * A) + " + " + Math.Sqrt(- delta) / (2 * A) + " i";
+                    x1 = -B / (2 * A) + " - " + Math.Sqrt(- delta) / (2 * A) + " i";
+                }
+
+                //preparar os dados a serem enviados para o browser
+                ViewBag.X1 = x1;
+                ViewBag.X2 = x2;
+
+            }
+            else
+            {
+
+            }
             return View();
         }
 
